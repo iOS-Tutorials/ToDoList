949a53490442ab38d807674cd5df672378a35b56
Alhaytham Alfeel <alhaythamalfeel@mac-machine.local>
2015-03-11 14:18:56 +0200

    New project

---
+++ ToDoList/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  ToDoList
+//
+//  Created by Alhaytham Alfeel on 2015-03-11.
+//  Copyright (c) 2015 NTG. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
